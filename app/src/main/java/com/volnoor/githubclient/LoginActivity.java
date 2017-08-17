49cbb52d6de6a0d368c58e933b4c07cb8bdb4843
Eugene <volnoor@gmail.com>
2017-08-17 13:55:29 +0300

    basic login

--- app/src/main/java/com/volnoor/githubclient/LoginActivity.java
+++ app/src/main/java/com/volnoor/githubclient/LoginActivity.java
@@ -2,12 +2,19 @@ package com.volnoor.githubclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signIn(View v) {
+        Log.d(TAG, "signIn");
     }
 }
