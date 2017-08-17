1180362e7e57d66a59c584eb06606760f25cfb34
Eugene <volnoor@gmail.com>
2017-08-17 15:55:03 +0300

    basic login#2

--- app/src/main/java/com/volnoor/githubclient/LoginActivity.java
+++ app/src/main/java/com/volnoor/githubclient/LoginActivity.java
@@ -1,20 +1,77 @@
 package com.volnoor.githubclient;
 
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private final String URL = "https://api.github.com/users/";
+
+    private EditText etUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.et_username);
     }
 
     public void signIn(View v) {
         Log.d(TAG, "signIn");
+
+        String username = etUsername.getText().toString();
+
+        new LoginTask().execute(URL + username);
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            JSONObject json = null;
+
+            try {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(strings[0])
+                        .build();
+                Response response = client.newCall(request).execute();
+                json = new JSONObject(response.body().string());
+            } catch (@NonNull IOException | JSONException e) {
+                Log.e(TAG, "" + e.getLocalizedMessage());
+            }
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            if (jsonObject != null) {
+
+                if (jsonObject.has("message")) {
+                    // Username doesn't exist
+                    Log.d(TAG, "message");
+                    return;
+                }
+
+                Log.d(TAG, jsonObject.toString());
+            }
+        }
     }
 }
